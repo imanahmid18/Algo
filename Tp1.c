3c6f280c9352845e9b78e06405ae9a19ab2b49db
imanahmid18 <imanahmid18@gmail.com>
2024-10-30 11:23:27 +0000

    Update Tp1.c

--- Tp1.c
+++ Tp1.c
@@ -1,4 +1,11 @@
-
+# include <stdio.h>
+int main () {
+     int  A [4] [4]  ={ {1, 5, 9, 13 } , {2, 6, 10, 14 } , {3, 7, 11, 15} , {4, 8, 12, 16} } ;
+int T , i , j ;
+    for(i=0 ;i<4; i++){
+       for(j=0; j<4;j++){
+           printf ("%d\t" , A[i][j]);
+            }
    printf("\n");
 } 
 
